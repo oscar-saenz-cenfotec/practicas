99359157070cbb3f51e0399ce97dc4e5f55fb3ce
oscar-saenz-cenfotec <60375101+oscar-saenz-cenfotec@users.noreply.github.com>
2020-02-20 21:14:21 -0600

    Creation of Biblioteca
    Creacion de practica

--- Semana4/Biblioteca/public/js/controlador.js
+++ Semana4/Biblioteca/public/js/controlador.js
@@ -1,4 +1,6 @@
-'use strict'
+'use strict';
+
+imprimir_lista_libros();
 
 let botonRegistrar = document.querySelector('#btnRegistrar');
 let inputTitulo = document.querySelector('#txtTitulo');
@@ -13,5 +15,25 @@ function obtenerDatos () {
     let nPrecio = Number(inputPrecio.value);
 
     registrar_libro (sTitulo, sEditorial, nPrecio);
+    imprimir_lista_libros();
+    /*console.log (sTitulo,sEditorial,nPrecio);*/
+    /*console.log('hola mundo');*/
+};
+function imprimir_lista_libros () {
+    let tbody = document.querySelector('#tblLibros tbody');
+    let lista_libros = obtener_lista_libros();
 
-}
+    tbody.innerHTML = '';   //se usa para limpiar la tabla previamente a ser llenada
+    
+    for(let i = 0; i < lista_libros.length; i++){
+        let fila = tbody.insertRow();
+
+        let celdaTitulo = fila.insertCell();
+        let celdaEditorial = fila.insertCell();
+        let celdaPrecio = fila.insertCell();
+
+        celdaTitulo.innerHTML = lista_libros[i][0];
+        celdaEditorial.innerHTML = lista_libros[i][1];
+        celdaPrecio.innerHTML = lista_libros[i][2];
+    }
+};
--- Semana4/Biblioteca/public/js/servicio.js
+++ Semana4/Biblioteca/public/js/servicio.js
@@ -7,3 +7,7 @@ function registrar_libro(psTitulo, psEditorial, pnPrecio){
 
     lista_libros.push(nuevoLibro);
 };
+
+function obtener_lista_libros (){
+    return lista_libros;
+}
